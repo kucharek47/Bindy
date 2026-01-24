33c2c654663bcfbfa9d36ed937c5fc78e57601c7
Kucharek47 <matzar0@wp.pl>
2026-01-24 02:09:08 +0100

    0.1.5

--- miejsce_klikniecia.py
+++
@@ -1,9 +0,0 @@
-from pynput import mouse
-
-def on_click(x, y, button, pressed):
-    if pressed:
-        print(f"Kliknięcie: {button} na pozycji ({x}, {y})")
-
-with mouse.Listener(on_click=on_click) as listener:
-    print("Nasłuchiwanie kliknięć myszy... (naciśnij Ctrl+C, aby zakończyć)")
-    listener.join()
